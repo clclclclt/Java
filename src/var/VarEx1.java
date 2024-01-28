07acbefff1185b2845db3ebe9ff9e3e3b60b7140
clclt <clclclclclt@naver.com>
2024-01-29 02:15:40 +0900

    Feat: 연산자/변수 문제 풀이

---
+++ src/var/VarEx1.java
@@ -0,0 +1,13 @@
+package var;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(4 + num2);
+        System.out.println(4 - num2);
+        System.out.println(4 * num2);
+
+    }
+}
