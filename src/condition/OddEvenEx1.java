43a9f1e590537ae8c90d0460d0dc21a5ae592624
clclt <clclclclclt@naver.com>
2024-01-29 03:44:10 +0900

    Feat: 연산자/조건문 문제 풀이

---
+++ src/condition/OddEvenEx1.java
@@ -0,0 +1,11 @@
+package condition;
+
+public class OddEvenEx1 {
+    public static void main(String[] args) {
+
+        int x= 1;
+
+        String num = x % 2 == 1 ? "홀수" : "짝수";
+        System.out.println(num);
+    }
+}
---
+++ src/operation/OperationEx3.java
@@ -0,0 +1,12 @@
+package operation;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+
+        int score = 100;
+
+        boolean result = score >= 80 && score <= 100;
+        System.out.println(result);
+
+    }
+}
