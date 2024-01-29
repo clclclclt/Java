c26530e021d8bcb784e0a5f4a6ff638554c55564
clclt <clclclclclt@naver.com>
2024-01-29 21:32:43 +0900

    Feat: Scanner 문제 풀이

---
+++ src/scanner/ScannerEx2.java
@@ -0,0 +1,24 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerEx2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("하나의 정수를 입력하세요:");
+
+        int num = sc.nextInt();
+        String result = "";
+
+        if(num%2 == 0 ){
+            result = "짝수";
+        } else {
+            result = "홀수";
+        }
+
+        System.out.println("입력한 숫자 "+num+"는 "+result+"입니다.");
+
+    }
+}
